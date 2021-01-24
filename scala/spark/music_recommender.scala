13312063529cc7aa15c6226e146baa3488c50682
chonnington <chonnington@gmail.com>
2021-01-24 10:59:13 -0500

    checking in artist_alias

--- scala/spark/music_recommender.scala
+++ scala/spark/music_recommender.scala
@@ -1,5 +1,4 @@
 
-
 val rawUserArtistData = spark.read.textFile("hdfs:///user/ds/user_artist_data.txt")
 
 rawUserArtistData.take(5).foreach(println)
@@ -10,3 +9,27 @@ val userArtistDF = rawUserArtistData.map { line =>
 }.toDF("user", "artist")
 
 userArtistDF.agg(min("user"), max("user"), min("artist"), max("artist")).show()
+
+val artistByID = rawArtistData.flatMap { line => 
+    val (id, name) = line.span(_ != '\t')
+    if (name.isEmpty) {
+        None
+    } else { 
+        try {
+            Some((id.toInt, name.trim)) 
+        } catch {
+            case _: NumberFormatException => None 
+        }
+    }
+}.toDF("id", "name")
+
+val rawArtistAlias = spark.read.textFile("hdfs:///user/ds/artist_alias.txt") 
+
+val artistAlias = rawArtistAlias.flatMap { line =>
+    val Array(artist, alias) = line.split('\t') 
+    if (artist.isEmpty) {
+        None
+    } else {
+        Some((artist.toInt, alias.toInt))
+    }
+}.collect().toMap
