225966745a3b0a79d2e1c4bc1f60226b877dc29c
chonnington <chonnington@gmail.com>
2021-01-31 09:32:31 -0500

    checking in

--- scala/sandbox/immutable_set.scala
+++ scala/sandbox/immutable_set.scala
@@ -1,10 +1,26 @@
+
+// Approach #1
+
 var jetSet = Set("Boeing", "Airbus")
+
 jetSet += "Lear"
+
 println(jetSet.contains("Cessna"))
+
+// Approach #2
 
 import scala.collection.mutable
 
 val movieSet = mutable.Set("Hitch", "Poltergeist")
+
 movieSet += "Shrek"
 
 println(movieSet)
+
+// Approach #3
+
+import scala.collection.immutable.HashSet
+
+val hashSet = HashSet("Tomatoes", "Chilies")
+
+println(hashSet + "Coriander")
