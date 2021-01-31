a0d6fd060d99dc284a3320eea135f9404f5d1b78
chonnington <chonnington@gmail.com>
2021-01-31 09:15:08 -0500

    adding gitignore and removing DS_STORE/.idea

--- scala/immutable_set.scala
+++ scala/immutable_set.scala
@@ -1,5 +1,9 @@
 var jetSet = Set("Boeing", "Airbus")
-
 jetSet += "Lear"
-
 println(jetSet.contains("Cessna"))
+
+import scala.collection.mutable
+
+val movieSet = mutable.Set("Hitch", "Poltergeist")
+movieSet += "Shrek"
+println(movieSet)
