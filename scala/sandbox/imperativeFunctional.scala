3fc8b01c06bf9b6dee3ab93f2ac22e78c613a951
chonnington <chonnington@gmail.com>
2021-02-01 10:17:25 -0500

    checking in

--- scala/sandbox/imperativeFunctional.scala
+++ scala/sandbox/imperativeFunctional.scala
@@ -22,3 +22,9 @@ def printArgs(args: Array[String]): Unit = {
 def printArgs(args: Array[String]): Unit = {
     args.foreach(println)
 }
+
+def formatArgs(args: Array[String]) = args.mkString("\n")
+
+val res = formatArgs(Array("zero", "one", "two"))
+
+assert(res == "zero\none\ntwo")
