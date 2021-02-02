da07a56cd811f8c3d9d7d099e872d3d15c77e0b7
chonnington <chonnington@gmail.com>
2021-02-02 10:34:57 -0500

    check in

--- scala/sandbox/classes_and_objects/checksumAccumulator.scala
+++ scala/sandbox/classes_and_objects/checksumAccumulator.scala
@@ -24,3 +24,13 @@ object ChecksumAccumulator {
                 cs
         } 
 }
+
+ChecksumAccumulator.calculate("Every value is an object.")
+
+/*
+
+A singleton object is more than a holder of static methods, however. 
+It is a first-class object. You can think of a singleton object’s name, 
+therefore, as a “name tag” attached to the object:
+
+*/
