7a12d7bed4003bc34437777d8a487873e7f4b1fa
chonnington <chonnington@gmail.com>
2021-02-02 09:58:13 -0500

    check

--- scala/sandbox/classes_and_objects/class_example.scala
+++ scala/sandbox/classes_and_objects/class_example.scala
@@ -13,3 +13,24 @@ class ChecksumAccumulator {
 
 val acc = new ChecksumAccumulator
 acc.sum = 5 // Won't compile, because sum is private
+
+class ChecksumAccumulator {
+
+    private var sum = 0
+
+    def add(b: Byte): Unit = {
+        sum += b
+    }
+
+    def checksum(): Int = { 
+        return ~(sum & 0xFF) + 1
+    } 
+
+}
+
+// In file ChecksumAccumulator.scala
+class ChecksumAccumulator {
+    private var sum = 0
+    def add(b: Byte): Unit = { sum += b } 
+    def checksum(): Int = ~(sum & 0xFF) + 1
+}
