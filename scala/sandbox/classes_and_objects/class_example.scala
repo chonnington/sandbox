308ae1294302dc9f22b75a349ebbe6a9ac4d814f
chonnington <chonnington@gmail.com>
2021-02-02 09:47:36 -0500

    check

--- scala/sandbox/classes_and_objects/class_example.scala
+++ scala/sandbox/classes_and_objects/class_example.scala
@@ -6,3 +6,10 @@ val acc = new ChecksumAccumulator
 val csa = new ChecksumAccumulator
 
 acc.sum = 3
+
+class ChecksumAccumulator {
+    private var sum = 0
+}
+
+val acc = new ChecksumAccumulator
+acc.sum = 5 // Won't compile, because sum is private
