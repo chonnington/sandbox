3e8e627d3c1b048be2a4e06836e28c44acf7403c
chonnington <chonnington@gmail.com>
2021-02-06 21:51:34 -0500

    check

--- scala/spark/loadSequenceFile.scala
+++ scala/spark/loadSequenceFile.scala
@@ -1,2 +1,9 @@
+
 val data = sc.sequenceFile(inFile, classOf[Text], classOf[IntWritable]).
     map{case (x, y) => (x.toString, y.get())}
+
+// save sequence file
+
+val data = sc.parallelize(List(("Panda", 3), ("Kay", 6), ("Snail", 2))) 
+
+data.saveAsSequenceFile(outputFile)
