f1b37ad5cf3a51f3e4dc93c90c7522e8df63c414
chonnington <chonnington@gmail.com>
2021-03-20 17:32:41 -0400

    check

---
+++ misc/aws/createCluster.java
@@ -0,0 +1,56 @@
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
+import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClientBuilder;
+import com.amazonaws.services.elasticmapreduce.model.*;
+import com.amazonaws.services.elasticmapreduce.util.StepFactory;
+
+public class Main {
+
+	public static void main(String[] args) {
+		AWSCredentials credentials_profile = null;		
+		try {
+			credentials_profile = new ProfileCredentialsProvider("default").getCredentials();
+        } catch (Exception e) {
+            throw new AmazonClientException(
+                    "Cannot load credentials from .aws/credentials file. " +
+                    "Make sure that the credentials file exists and the profile name is specified within it.",
+                    e);
+        }
+        
+        AmazonElasticMapReduce emr = AmazonElasticMapReduceClientBuilder.standard()
+			.withCredentials(new AWSStaticCredentialsProvider(credentials_profile))
+			.withRegion(Regions.US_WEST_1)
+			.build();
+        
+        // create a step to enable debugging in the AWS Management Console
+		StepFactory stepFactory = new StepFactory(); 
+		StepConfig enabledebugging = new StepConfig()
+   			.withName("Enable debugging")
+   			.withActionOnFailure("TERMINATE_JOB_FLOW")
+   			.withHadoopJarStep(stepFactory.newEnableDebuggingStep());
+        
+        Application spark = new Application().withName("Spark");
+
+        RunJobFlowRequest request = new RunJobFlowRequest()
+            .withName("Spark Cluster")
+            .withReleaseLabel("emr-5.20.0")
+            .withSteps(enabledebugging)
+            .withApplications(spark)
+            .withLogUri("s3://path/to/my/logs/")
+	       	.withServiceRole("EMR_DefaultRole") 
+	       	.withJobFlowRole("EMR_EC2_DefaultRole") 
+            .withInstances(new JobFlowInstancesConfig()
+                .withEc2SubnetId("subnet-12ab3c45")
+                .withEc2KeyName("myEc2Key")
+                .withInstanceCount(3)
+                .withKeepJobFlowAliveWhenNoSteps(true)
+                .withMasterInstanceType("m4.large")
+                .withSlaveInstanceType("m4.large")
+            );			
+        RunJobFlowResult result = emr.runJobFlow(request);  
+	    System.out.println("The cluster ID is " + result.toString());
+	}
+}
