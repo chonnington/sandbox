df33baca7b1590a4369d7f36af96385a59138e0a
chonnington <chonnington@gmail.com>
2021-03-21 08:38:31 -0400

    checking in

--- language/scala/sandbox/rational.scala
+++ language/scala/sandbox/rational.scala
@@ -1,3 +1,18 @@
 class Rational(n: Int, d: Int) {
     println("Created " + n + "/" + d)
 }
+
+new Rational(1, 2)
+
+// scala> new Rational(1, 2)
+// Created 1/2
+// res0: Rational = Rational@2591e0c9
+
+class Rational(n: Int, d: Int) {
+    override def toString = n + "/" + d
+}
+
+// scala> val x = new Rational(1, 3)
+// x: Rational = 1/3
+// scala> val y = new Rational(5, 7)
+// y: Rational = 5/7
