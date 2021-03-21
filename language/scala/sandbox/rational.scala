2510030dc52006557e02bad155af81ea7cbf58e4
chonnington <chonnington@gmail.com>
2021-03-21 17:11:57 -0400

    augment check rational class scala

--- language/scala/sandbox/rational.scala
+++ language/scala/sandbox/rational.scala
@@ -69,3 +69,26 @@ class Rational(n: Int, d: Int) {
 
 // scala> val y = new Rational(3)
 // y: Rational = 3/1
+
+ class Rational(n: Int, d: Int) {
+
+     require(d != 0)
+
+     private val g = gcd(n.abs, d.abs)
+
+     val numer = n / g
+     val denom = d / g
+
+     def this(n: Int) = this(n, 1)
+
+     def add(that: Rational): Rational =
+        new Rational(
+            numer * that.denom + that.numer * denom,
+            denom * that.denom
+       )
+
+     override def toString = numer + "/" + denom
+
+     private def gcd(a: Int, b: Int): Int =
+        if (b == 0) a else gcd(b, a % b)
+}
